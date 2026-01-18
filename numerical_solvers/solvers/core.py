e64c4f59e4a2510a1d0c5d3fc47352a612988191
Cian Quezon <cdsquezon@gmail.com>
2026-01-18 23:29:35 +0000

    Ruff formatted it

--- numerical_solvers/solvers/core.py
+++ numerical_solvers/solvers/core.py
@@ -4,7 +4,6 @@ Core interface for Solvers
 Author: Cian Quezon
 """
 
-
 from meteorological_equations.math.solvers._enums import SolverName
 from meteorological_equations.math.solvers._solvers import (
     BisectionSolver,
