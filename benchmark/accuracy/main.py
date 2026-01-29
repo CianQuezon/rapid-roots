d2438f015de053d1ef10de4fd3fc61f1c8802298
Cian Quezon <cdsquezon@gmail.com>
2026-01-29 21:19:08 +0000

    removed unused imports

--- benchmark/accuracy/main.py
+++ benchmark/accuracy/main.py
@@ -2,17 +2,12 @@
 Main code for running accuracy benchmark
 """
 import sys
-import time
 from pathlib import Path
-from typing import Dict
 
 sys.path.append(str(Path(__file__).parent.parent))
 
-from accuracy._benchmark import run_benchmark_single_function
 from accuracy._plot import plot_error_distribution_boxplot
-from shared.functions import FUNCTIONS_LIST
 from accuracy._benchmark import run_functions_accuracy_benchmark
-
 
 
 def main():
